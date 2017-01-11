b74ee0185a95d4d8e1f8d35a5dce0b759c773026
Parker Bannister <p.bannister123@gmail.com>
2017-01-10 18:15:33 -0800

    Add completed assignment

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -4,13 +4,29 @@
 # the sentence of the form
 #   "The difference in length is N"
 
+CompareLength <- function(String1, String2) {
+  difference <- abs(nchar(String1) - nchar(String2))
+  return(paste("The difference in length is", difference))
+}
+
+
 
 # Pass two strings of different lengths to your `CompareLength` function
+CompareLength("me", "parker")
 
 
 # Define a function `DescribeDifference` that will return one of the following statements:
 # "Your first string is longer by N characters"
 # "Your second string is longer by N characters"
-
+DescribeDifference <- function(String1, String2) {
+  if(nchar(String1) > nchar(String2)) {
+    difference <- abs(nchar(String1) - nchar(String2))
+    return(paste("Your first string is longer by", difference,"characters"))
+  } else {
+    difference <- abs(nchar(String2) - nchar(String1))
+    return(paste("Your second string is longer by", difference,"characters"))
+  }
+}
 
 # Pass two strings of different lengths to your `DescribeDifference` function
+DescribeDifference("parker", "me")
---
+++ module6-functions/exercise-2/exercise.R
@@ -0,0 +1,16 @@
+# Exercise 2: writing and executing functions (II)
+
+# Define a function `CompareLength` that takes in 2 character strings, and returns
+# the sentence of the form
+#   "The difference in length is N"
+
+
+# Pass two strings of different lengths to your `CompareLength` function
+
+
+# Define a function `DescribeDifference` that will return one of the following statements:
+# "Your first string is longer by N characters"
+# "Your second string is longer by N characters"
+
+
+# Pass two strings of different lengths to your `DescribeDifference` function
